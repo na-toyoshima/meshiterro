3dd64cf206895be860480c3f143393fb7ef9a9b8
na-toyoshima <na.toyoshim@gmail.com>
2020-03-25 19:51:06 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd3f00439acd26de04c02eb59a68e5e63d6f0f789be1896eafff4441a003afe4d845ca0728b7b45dc0cb6c4f1dc4218516ee4b6ba99d8a80f6e4bd5044c78ecee'
