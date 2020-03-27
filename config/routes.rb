653182a365462b8c3becd40e58de8f398112dfd2
na-toyoshima <na.toyoshim@gmail.com>
2020-03-27 20:57:22 +0900

    [finish]5-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'post_images#index'
 
@@ -6,9 +7,12 @@ Rails.application.routes.draw do
   	resource :post_comments, only: [:create]
   end
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only:[:show, :edit, :update] do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
